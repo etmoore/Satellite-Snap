da20ad1afa38122ce654196188f82443221b2058
Evan Moore <etmoore@gmail.com>
2014-06-19 22:11:26 -0600

    added div for satellite photo

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,19 @@
 function initialize() {
   var mapOptions = {
     center: new google.maps.LatLng(39.916015, -104.984253),
-    zoom: 8
+    zoom: 9
   };
   var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 }
+
 google.maps.event.addDomListener(window, 'load', initialize);
+
+var app = {
+  init: function() {
+    alert("Hello!");
+  }
+};
+
+$(function() {
+  app.init();
+})

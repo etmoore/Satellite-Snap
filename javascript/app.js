12c47bf91f162684be0f9395f98b784057cb0cc5
Evan Moore <etmoore@gmail.com>
2014-06-19 22:13:27 -0600

    renamed initialize function  to loadMap

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,4 @@
-function initialize() {
+function loadMap() {
   var mapOptions = {
     center: new google.maps.LatLng(39.916015, -104.984253),
     zoom: 9
@@ -6,11 +6,11 @@ function initialize() {
   var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', loadMap);
 
 var app = {
   init: function() {
-    alert("Hello!");
+
   }
 };
 
